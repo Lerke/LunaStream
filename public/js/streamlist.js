24728cebdc469e39b2f4121bba070106ceb76ea4
Lerke <lerke@berrypunch.net>
2015-12-04 04:26:17 +0100

    fix

--- public/js/streamlist.js
+++ public/js/streamlist.js
@@ -6,11 +6,7 @@ jQuery(window).ready(function () {
 		jQuery("#streamcontainer").append("<div class=\"streamblock\"><div id=\"stream-"+stream+"\" class=\"streamblockcontent\"<div class=\"streamtitle\"><div class=\"titlediv\">" + StreamHost.streams[stream].name + "</div><div class=\"streaminfo\"></div></div></div>");
 		jQuery("#stream-"+stream).css('background-image', 'url(' + prefix + servername + servercache + StreamHost.streams[stream].name + '.png' + ')');
 		//><img id=\"img\" src=" + prefix + servername + servercache + StreamHost.streams[stream].name + ".png" + "></img>
-<<<<<<< HEAD
-			jQuery(".titlediv").blurjs({
-=======
 			jQuery(".streamtitle").blurjs({
->>>>>>> 49a581b7e1d0d87889ca1d99a8bdaa394f543429
 			source: '#stream-' + stream,
 		overlay: 'rgba(255,255,255,0.4)',
 	radius: 10
